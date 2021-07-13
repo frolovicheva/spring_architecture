586d8f584866a2c55f7318c0f10935b92dd8d12d
frolovicheva <loran-azs@mail.ru>
2021-07-13 09:41:30 +0300

    finished home work

---
+++ src/main/java/ru/geekbrains/spring/one/model/Product.java
@@ -0,0 +1,41 @@
+package ru.geekbrains.spring.one.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private int price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public static void main(String[] args) {
+        Product product = new Product (1L,"pop", 90, new Category (1L, "food"));
+    }
+}
